7c4d7c318a9d4b7ef2df6b3f72be2d4970971be7
Ayusman Parida <paridaayusman152@gmail.com>
2023-08-27 13:20:01 +0530

    added navbar

---
+++ client/src/Theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const Theme = createTheme({
+  palette:{
+    primary:{
+      main: "#dc3530",
+      light: "skyblue"
+    },
+    secondary:{
+      main: '#fff',
+    },
+    otherColor:{
+      main:"#999"
+    },
+    white:{
+      main:'#fff',
+    }
+  }
+})
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { Theme } from './Theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <ThemeProvider theme={Theme}>
     <App />
+    </ThemeProvider>
     </BrowserRouter>,
 )
