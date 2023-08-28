dd43f8f4ad1ec7a31fca4e7b11209e9aa6e96d87
skniyaznoor <skniyaznoor23@gmail.com>
2023-08-29 01:26:20 +0530

    AllRoutes updated

--- client/src/components/AllRoutes.jsx
+++ client/src/components/AllRoutes.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Test from './Test'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Test from "./Test";
+import Login from "../pages/Login";
+import Footer from "./Footer";
+
 const AllRoutes = () => {
   return (
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/test1' element={<Test/>}/>
-    </Routes>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test1" element={<Test />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
