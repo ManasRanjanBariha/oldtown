93bdab3b60439e5f97d86a3d0f9df61f604a3c14
skniyaznoor <skniyaznoor23@gmail.com>
2023-08-29 01:23:55 +0530

    login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,24 +2,26 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div class="loginbox">
-        <div class="container">
-            <div class="form">
-                <h1>Hi there!</h1>
-            <div class="inside">
-            <label class="label">SignIn or SignUp</label>
-            <input
-             placeholder="Mobile Number*"
-             type="text"
-             name="newnumber"
-             autocomplete="off"
-              id="login-input"
-             class="textinputbox"
-            ></input>
+    <>
+      <div className="loginbox">
+        <div className="container">
+          <div className="form">
+            <h1>Hi there!</h1>
+            <div className="inside">
+              <label className="label">SignIn or SignUp</label>
+              <input
+                placeholder="Mobile Number*"
+                type="text"
+                name="newnumber"
+                autocomplete="off"
+                id="login-input"
+                className="textinputbox"
+              ></input>
             </div>
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </>
   );
 };
 
